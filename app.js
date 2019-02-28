bee30d94b49134f4bf77a950c9fcf780e265aa6d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-28 00:51:26 -0600

    Fix font awesome style link. Create document variables. Create addTask().

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+// DEFINE UI VARS
+const form = document.getElementById('task-form');
+const taskList = document.querySelector('ul.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.getElementById('filter');
+const taskInput = document.getElementById('task');
 
+// LOADS THE EVENT LISTEENERS FOR THE PAGE
+loadEventListeners();
+
+function loadEventListeners() {
+	//ADD TASK EVENT
+	form.addEventListener('submit', addTask);
+}
+
+//ADD TASK FUNCTION
+function addTask(e) {
+	if(taskInput.value === ''){
+		alert('Please add a task first.');
+	}
+
+	//If we have input, CREATE LI ELEMENT
+	const li = document.createElement('li');
+	//this adds styling from Materalize.css
+	li.className = 'collection-item';
+	// Create text node and append to the li
+	li.appendChild(document.createTextNode(taskInput.value));
+	// Create new link element
+
+	//create the 'delete' icon element link
+	const link = document.createElement('a');
+	// secondary-content places to the right of an item in materialze.css
+	link.className = 'delete-item secondary-content';
+	link.innerHTML = '<i class="far fa-trash-alt"></i>';
+	li.appendChild(link);
+
+	//now, append the li to the ul
+	taskList.appendChild(li);
+
+	//clear the input
+	taskInput.value = '';
+	e.preventDefault(); //prevent form submit
+}
